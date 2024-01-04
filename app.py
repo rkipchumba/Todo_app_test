01b9c7d1fdba0233c1192938c3d5e78b3def79c3
Rodgers <98005951+rkipchumba@users.noreply.github.com>
2024-01-04 20:01:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# /// = relative path, //// = absolute path
+# ///  = relative path, //// = absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
